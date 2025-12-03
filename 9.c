27ebefd6817af9717905e266aff8c64a182ce8da
Maayukh Misra <maayukhmisra@gmail.com>
2025-12-03 13:10:17 +0530

    Array Rotation

--- 9.c
+++ 9.c
@@ -1,17 +1,32 @@
 #include<stdio.h>
-int main(){
-    int arr[10]={1,2,3,4,5,6,7,8,9,10};
+
+int main() {
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
     int rotate;
-    int n=sizeof(arr) / sizeof(arr[0]);
-    printf("\n Enter your number ...");
-    scanf("%d",&rotate);
-    rotate=rotate%n;
-    for(int i=0;i<rotate;i++){
-        int temp=arr[i];
-        arr[i]=arr[(i+rotate)%n];
-        arr[(i+rotate)%n]=temp;
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    printf("\nEnter rotation count: ");
+    scanf("%d", &rotate);
+
+    rotate = rotate % n; 
+
+    int temp[n];
+
+    for(int i = 0; i < rotate; i++) {
+        temp[i] = arr[n - rotate + i];
     }
-    for(int i=0;i<n;i++){
-        printf("%d",arr[i]);
+
+    for(int i = rotate; i < n; i++) {
+        temp[i] = arr[i - rotate];
     }
+
+    for(int i = 0; i < n; i++) {
+        arr[i] = temp[i];
+    }
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
 }
